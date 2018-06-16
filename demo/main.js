b3de39e962e3cda7b537ece2f80298c9b92e38dd
myronliu347 <myronliu347@gmail.com>
2018-06-17 02:32:22 +0800

    v0.1.0

--- demo/main.js
+++ demo/main.js
@@ -1,7 +1,7 @@
 import 'muse-ui/dist/muse-ui.css';
 import Vue from 'vue';
 import MuseUI from 'muse-ui';
-import MuseUIToast from '../src/index';
+import MuseUIToast from '../dist/muse-ui-toast.esm';
 import App from './App';
 Vue.use(MuseUI);
 Vue.use(MuseUIToast);
